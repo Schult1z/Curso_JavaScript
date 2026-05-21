72674324eafaf875524ead17a2d17c6d0f3affaf
Gabriel Schultz do Amaral <gab12schultz@hotmail.com>
2026-05-21 15:47:55 -0300

    Update scripts.js

--- 1_introducao/scripts.js
+++ 1_introducao/scripts.js
@@ -1,4 +1,4 @@
-console.log('Hello world script.js !'); //console.log imprime algo na tela
+console.log("Hello world script.js !"); //console.log imprime algo na tela
 
 console.log(typeof 10);
 console.log(typeof 12.7);
@@ -6,7 +6,7 @@ console.log(typeof -10);
 
 // operacoes aritimeticas
 
-console.log(typeof(10 + 10));
+console.log(typeof (10 + 10));
 console.log(10 - 5);
 console.log(10 * 10);
 console.log(10 / 2);
@@ -19,15 +19,15 @@ console.log(-Infinity);
 console.log(NaN);
 
 // strings
-console.log(`Gabriel`);// isso e uma string(texto)
+console.log(`Gabriel`); // isso e uma string(texto)
 console.log(`uma linha \n segunda linha`);
-console.log(`2 + 2 e: ${2+2}`);
-console.log(`meu `+`nome `+`Gabriel`);
+console.log(`2 + 2 e: ${2 + 2}`);
+console.log(`meu ` + `nome ` + `Gabriel`);
 
 // Booleans (True or False)
 
-console.log(5<2); // resultado: False
-console.log(5>2); // resultado: True
+console.log(5 < 2); // resultado: False
+console.log(5 > 2); // resultado: True
 
 console.log(typeof true);
 console.log(typeof false);
@@ -48,5 +48,20 @@ console.log(null);
 // Conversao automatica
 console.log(5 * null); // 0
 console.log(`5` - 3); // 2
-console.log(`5`+ 1); // 51
-console.log9(2 * `tres`) // NaN
+console.log(`5` + 1); // 51
+console.log(2 * `tres`); // NaN
+
+// Variaveis (Guarda um valor na memoria)
+let name = `Gabriel`;
+
+console.log(name);
+console.log(`Meu nome e ${name}!`);
+
+let number = 5;
+console.log(number * number);
+
+let um = 1, dois = 2, tres = 3;
+console.log(um, dois, tres);
+
+const lastName = `Schultz`; //valor em const nao pode ser alterado
+console.log(lastName);
