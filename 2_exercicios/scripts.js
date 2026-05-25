463c8c2e5d4c5d724c5136b3a48299dbb538348e
Gabriel Schultz do Amaral <gab12schultz@hotmail.com>
2026-05-25 15:45:54 -0300

    Update scripts.js

--- 2_exercicios/scripts.js
+++ 2_exercicios/scripts.js
@@ -27,3 +27,102 @@
 /* 5)
     console.log(5 * "Gabriel") // NaN
 */
+
+/* 6)
+    let name = `Gabriel`;
+    let years = `20`;
+    let autorizado = true;
+
+    console.log(typeof name);
+    console.log(typeof years);
+    console.log(typeof autorizado);
+*/
+
+/* 7)
+    let idade = 20;
+    if (idade >= 18) {\
+        console.log(Voce tem ${idade}, pode entrar)
+    } else {
+        console.log(`Voce nao tem a idade suficiente!)
+        }
+*/
+
+/* 8)
+    const name = `Gabriel`;
+
+    if (name == `Gabriel`) {
+        console.log(`Oi, ${name}`);
+    } else {
+        console.log(`Nome nao encontrado`);
+        }
+*/
+
+/* 9) Math.pow
+    console.log(Math.pow(2, 2));
+    console.log(Math.pow(3, 2));
+    console.log(Math.pow(18 , 2));
+*/
+
+/* 10)
+    let velocidade = 120;
+
+    if( velocidade <= 80) {
+        console.log(`Voce passou no limite permitido);
+    } else {
+        console.log(`Levou multa por ultrapassar a velocidade permitida`);
+        }
+*/
+
+/* 11)
+    let idade = 19;
+    let cnh = true;
+
+    if(idade >= 18 && cnh == true) {
+        console.log(`Permitido dirigir`)
+    }else if (idade >= 18 && cnh == false) {
+        console.log(`Voce nao possui CNH`); 
+    } else {
+        console.log(`Nao esta permitido dirigir ainda!`);
+        }
+*/
+
+/* 12)
+    let number = 0;
+    while (number <= 10) {
+        console.log(`O number vale: ${number}`);
+        number = number + 1;
+    }
+*/
+
+/* 13)
+    for (i = 100; i >= 50; i = i - 1) {
+        console.log(`Valor de i: ${i}`);
+    }
+*/
+
+/* 14)
+    for (a = 0; a <= 50; a++ ) {
+        if(a % 2 == 0) {
+            console.log(`Este numero e par: ${a}`);
+        } else {
+            console.log(`Este numero e impar: ${a}`);
+            }
+    }
+*/
+
+/* 15)
+    let num = 41;
+    let divisoes = 0;
+
+    for( let i = 1; i <= num; i ++) {
+        if (num % i == 0) {
+        divisoes++;
+        }
+    }
+
+    if (divisoes == 2) {
+        console.log(`Este numero primo: ` + num);
+    } else {
+        console.log(`Este numero nao e primo: ` + num);
+        }
+*/
