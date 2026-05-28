b2b1e876a83a45fb75cee4501af7905b702f4bdc
Gabriel Schultz do Amaral <gab12schultz@hotmail.com>
2026-05-28 13:58:16 -0300

    Create Array

---
+++ 4_array/scripts.js
@@ -0,0 +1,22 @@
+// Arrays
+let arr = [1,2,3,4,5];
+let nomes = [`Gabriel`, `Joao`, `Arthur`, `Ricardo`];
+let bool = [true, false, true, false];
+let misturado = [1, `Amaral`, true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
+
+// Chamando valores da array, comeca do 0 ao 9
+let alunos = [`Joao`, `Amaral`, `Cloves`];
+console.log(alunos[2]); // vai aparecer o Cloves
+console.log(arr[arr.length - 1]); // para acessar o ultimo valor do array
+
+// Metodos (transformar os dados)
+let marca = 'Nike';
+console.log(marca.toUpperCase());
+console.log(typeof marca.toUpperCase());
+//.propriedade;
+// metodo();
