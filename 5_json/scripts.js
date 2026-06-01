419a125e671ec4ceaeb3932c41c14d2b73849bc7
Gabriel Schultz do Amaral <gab12schultz@hotmail.com>
2026-06-01 12:21:59 -0300

    Update scripts.js

--- 5_json/scripts.js
+++ 5_json/scripts.js
@@ -6,3 +6,19 @@ let pessoa = {
 
 console.log(pessoa.nome);
 console.log(pessoa.idade);
+
+// convertendo JSON para string
+let pessoa = {
+    "nome": "Gabriel",
+    "idade": 28,
+    "profissao": "Programador",
+    "hobbies": ["Video Games", "Futebol"]
+}
+
+let pessoaTexto = JSON.stringify(pessoa); // converte o JSON em uma string 
+console.log(pessoaTexto);
+
+let pessoaJSON = JSON.parse(pessoaTexto); // converte a string em JSON
+console.log(pessoaJSON);
+
+console.log(pessoaJSON.hobbies[0]);
